02f36509a27c23b31eab9c5445bb52d270431cd6
Abdeldjalil Fortas <dr.fcmam5@gmail.com>
2022-12-22 00:06:26 +0100

    :bug: refactor and add GH Actions

--- main.tf
+++ main.tf
@@ -1,11 +1,12 @@
 locals {
-  song_urls = { for song in var.songs : song.url => song }
+  song_urls = [for song in var.songs : song.url]
   song_ids  = [for song in data.spotify_track.lazy_songs : song.id]
 }
 
 data "spotify_track" "lazy_songs" {
-  for_each = local.song_urls
-  url      = each.value.url
+  for_each = toset(local.song_urls)
+
+  url = each.key
 }
 
 resource "spotify_playlist" "the-lazy-playlist" {
--- provider.tf
+++ provider.tf
@@ -1,10 +1,20 @@
 terraform {
+  cloud {
+    organization = "playground-dev"
+
+    workspaces {
+      name = "spotify"
+    }
+  }
+
   required_providers {
     spotify = {
       source  = "conradludgate/spotify"
       version = "~> 0.2.7"
     }
   }
+
+
 }
 
 provider "spotify" {
